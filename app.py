5faea7f153e25adfcde9e748823d5c3385ebdb78
aarvinrathod <aarvinrathod@gmail.com>
2021-10-25 21:49:26 -0500

    sqlfile

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, jsonify, redirect, request
+import pickle
+import numpy as np
+
+with open('models/clr0.sav', 'rb') as f: 
+	scaler=pickle.load(f)
+
+with open('models/scaler.sav', 'rb') as f: 
+	scaler=pickle.load(f)
 
 app = Flask(__name__)
 
@@ -9,8 +17,12 @@ def index():
 @app.route('/send', methods=['POST'])
 def choices():
     if request.method=='POST':
-       SprintSpeed= request.form['SprintSpeed']
+       Defending= request.form['Defending']
        Composure= request.form['Composure']
+       Positioning=requests.form['Positioning']
+       BallControl=requests.form['BallControl']
+       PaceTotal=requests.form['PaceTotal']
+       ShortPassing=requests.form['ShortPassing']
        #predictions go here (all selections into array of arrays)
        #load model (pickle.load)
        #model.predict (store model into variable)
